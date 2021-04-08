69e4239d4236f8ac6ecb29bf02ca278f6ff092e3
DEEP <deepsaha288@yahoo.com>
2021-04-08 16:36:59 +0530

    employee using function get working hours

--- employeepayroll.js
+++ employeepayroll.js
@@ -3,17 +3,20 @@ const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
+function getWorkingHours(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            empCheck=PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            empCheck=FULL_TIME_HOURS;
+            break;
+        default:
+            empHrs=4;
+    } 
+}
 let empHrs=4;
 empCheck =Math.floor(Math.random()*10)%3;
-switch(empCheck){
-    case IS_PART_TIME:
-        empCheck=PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empCheck=FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs=4;
-}
+empHrs=getWorkingHours(empCheck);
 let empWage=empHrs*WAGE_PER_HOUR;
 console.log("Employee Wage:"+empWage);
