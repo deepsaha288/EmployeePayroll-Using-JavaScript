43fbb476cbe99ea947072af36de76003f2a0c40d
DEEP <deepsaha288@yahoo.com>
2021-04-08 17:05:42 +0530

    calculting Wages of a month and working days

--- employeepayroll.js
+++ employeepayroll.js
@@ -3,8 +3,12 @@ const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
-const NUM_OF_WORKING_DAYS=2;
+const MAX_HRS_IN_MONTH=100;
+const NUM_OF_WORKING_DAYS=10;
 
+let empHrs=4;
+let MAX_HRS_IN_MONTH=0;
+let NUM_OF_WORKING_DAYS==0;
 function getWorkingHours(empCheck){
     switch(empCheck){
         case IS_PART_TIME:
@@ -19,10 +23,12 @@ function getWorkingHours(empCheck){
     return empHrs;
     
 }
-let empHrs=4;
-for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
-    empCheck =Math.floor(Math.random()*10)%3;
-    empHrs +=getWorkingHours(empCheck)
-    let empWage=empHrs*WAGE_PER_HOUR;
-    console.log("Employee Wage:"+empWage);
+
+while(totalEmpHrs <=MAX_HRS_IN_MONTH &&totalWorkingDays <=MAX_HRS_IN_MONTH){
+       totalWorkingdDays++;
+    let empCheck =Math.floor(Math.random()*10)%3;
+    totalEmpHrs +=getWorkingHours(empCheck);
+}
+    let empWage=totalEmpHrs * Wage_PER_HOUR;
+    console.log("TOTAL DAYS:"+totalWorkingDays +"total days:"+totalEmpHrs +" Emp Wage"+empWage);
 }
