889cdc92f4a24fd4ebe04bc56e464b4aff19ae52
DEEP <deepsaha288@yahoo.com>
2021-04-12 13:50:41 +0530

    calculated daily wage using Arrow sign

--- employeepayroll.js
+++ employeepayroll.js
@@ -33,28 +33,36 @@ function dayCount(dailwage){
     return dayCounter +" = "+dailwage;
 }
 
+
 function getFulltimwage(dailwage){
     return dailwage.includes("160");
 }
+
 
 function getPartTime(dailwage){
     return dailwage.includes("80");
 }
 
+const findTotal = (totalVal,dailyVal) => {
+    return totalVal+dailyVal;
+}
+
 let emphrs=0;
 let totalWorkinghrs=0;
 let totalworkingDays=0;
+let empHrsArr=[];
 
 while(totalWorkinghrs<=TOTAL_WORKING_HRS && totalworkingDays<MAX_NUMBER_OF_WORKING_DAYS){
     totalworkingDays++;
     let empcheck=Math.floor((Math.random()*10)%3);
     emphrs=getempHrs(empcheck);
     totalWorkinghrs+=emphrs;
+    empHrsArr.push(emphrs);
     dailWage.push(calcWage(emphrs));
 }
 
-dailWage.forEach(sum);
 
+dailWage.forEach(sum);
 let mapDayWithWage=dailWage.map(dayCount);
 console.log("Day with employee wage: "+mapDayWithWage);
 
@@ -62,6 +70,7 @@ console.log("Daily wage is: "+dailWage);
 console.log("Total Hours: "+totalWorkinghrs);
 console.log("Total Working Days: "+totalworkingDays);
 console.log("Daily Wage of a employee is: "+empWage);
+
 
 let fulltimeWage=mapDayWithWage.filter(getFulltimwage);
 console.log("Days where is full time wage: ");
@@ -73,4 +82,26 @@ console.log("Day where it is full time wage: "+mapDayWithWage.find(getFulltimwag
 
 console.log("Check All Element have Full Time Wage "+fulltimeWage.every(getFulltimwage));
 
+
 console.log("Check for Part time wage: "+mapDayWithWage.some(getPartTime));
+
+
+let count=0;
+let totalHrs=Array.from(mapDayWithWage.values()).reduce(findTotal,0);
+let totalSalary=dailWage.filter(dailwage => dailwage>0).reduce(findTotal,0);
+console.log("Emp Wage wit Arrow "+"Total hrs: "+totalHrs+"Total Salary: "+totalSalary);
+
+
+let nonWorkingDays=[];
+let partWorkingDays=[];
+let fullWorkingDays=[];
+
+empHrsArr.forEach((value,key,map) => {
+    if(value == 8) fullWorkingDays.push(key);
+    else if(value == 4)partWorkingDays.push(key);
+    else nonWorkingDays.push(key);
+});
+
+console.log("Full working Days are: "+fullWorkingDays);
+console.log("Part working Days are: "+partWorkingDays);
+console.log("Non working Days are: "+nonWorkingDays);
