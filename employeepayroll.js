327119676440ea735eabd6f4f64ae9c9ce5c1f4f
DEEP <deepsaha288@yahoo.com>
2021-04-12 13:45:18 +0530

    daily wage along with total wage using Array

--- employeepayroll.js
+++ employeepayroll.js
@@ -1,37 +1,77 @@
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2
-const PART_TIME_HOURS = 4;
-const FUL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20;
-const MAX_HRS_IN_MONTH = 160;
-const Num_OF_WORKING_DAYS = 20;
-let totalEmpHrs = 0;
-let totalWorkingDays = 0;
-let empHrs = 0;
-let empDailyWageArr = new Array();
+const IS_PARTTIME=1;
+const IS_FULLTIME=2;
+const FULLTIME_HRS=8;
+const PARTTIME_HRS=4;
+const WAGE_PER_HR=20;
+const MAX_NUMBER_OF_WORKING_DAYS=20;
+const TOTAL_WORKING_HRS=100;
+var dailWage=[];
 
-function calDailyWage(empHrs) {
-    return empHrs * WAGE_PER_HOUR;
-}
-function getWorkingHours(empCheck) {
-    switch (empCheck) {
-
-        case IS_PART_TIME:
-            return PART_TIME_HOURS;
-        case IS_FULL_TIME:
-            return FUL_TIME_HOURS;
+function getempHrs(empcheck){
+    switch(empcheck){
+        case 1:
+            return PARTTIME_HRS;
+        case 2:
+            return FULLTIME_HRS;
         default:
             return 0;
     }
 }
 
-while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < Num_OF_WORKING_DAYS) {
-    totalWorkingDays++;
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    let empHrs = getWorkingHours(empCheck);
-    totalEmpHrs += empHrs;
-    empDailyWageArr.push(calDailyWage(empHrs));
+function calcWage(totalWorkinghrs){
+    return totalWorkinghrs*WAGE_PER_HR;
 }
 
-let empWage = calDailyWage(totalEmpHrs);
-console.log("Total Days " + totalWorkingDays + " Total Hrs " + totalEmpHrs + " Emp Wage " + empWage);
+let empWage=0;
+
+function sum(dailwage){
+    return empWage+=dailwage;
+}
+
+let dayCounter=0;
+function dayCount(dailwage){
+    dayCounter++;
+    return dayCounter +" = "+dailwage;
+}
+
+function getFulltimwage(dailwage){
+    return dailwage.includes("160");
+}
+
+
+function getPartTime(dailwage){
+    return dailwage.includes("80");
+}
+
+let emphrs=0;
+let totalWorkinghrs=0;
+let totalworkingDays=0;
+
+while(totalWorkinghrs<=TOTAL_WORKING_HRS && totalworkingDays<MAX_NUMBER_OF_WORKING_DAYS){
+    totalworkingDays++;
+    let empcheck=Math.floor((Math.random()*10)%3);
+    emphrs=getempHrs(empcheck);
+    totalWorkinghrs+=emphrs;
+    dailWage.push(calcWage(emphrs));
+}
+
+
+dailWage.forEach(sum);
+
+let mapDayWithWage=dailWage.map(dayCount);
+console.log(mapDayWithWage);
+
+console.log("Daily wage is: "+dailWage);
+console.log("Total Hours: "+totalWorkinghrs);
+console.log("Total Working Days: "+totalworkingDays);
+console.log("Daily Wage of a employee is: "+empWage);
+
+let fulltimeWage=mapDayWithWage.filter(getFulltimwage);
+console.log("Days where is full time wage: ");
+console.log(fulltimeWage);
+
+console.log("Day where it is full time wage: "+mapDayWithWage.find(getFulltimwage));
+
+console.log("Check All Element have Full Time Wage "+fulltimeWage.every(getFulltimwage));
+
+console.log("Check for Part time wage: "+mapDayWithWage.some(getPartTime));
